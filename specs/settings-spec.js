2f0d5c4542e6041454e664833b1b9947972c6b8a
Benedikt Rötsch <benedikt@contentful.com>
2017-12-20 10:08:41 +0100

    fix(settings): locate form items by label and detect required attr

--- specs/settings-spec.js
+++ specs/settings-spec.js
@@ -1,3 +1,11 @@
+function formItemByLabel (label) {
+  return cy
+    .get('label')
+    .contains(label)
+    .parents('.form-item')
+    .find('input')
+}
+
 describe('The Example App', () => {
   context('Settings', () => {
     beforeEach(() => {
@@ -12,55 +20,53 @@ describe('The Example App', () => {
         .invoke('text').then((text) => {
           expect(text).to.contain('Connected space:')
         })
-      cy.get('input#input-space-id').should('have.value', Cypress.env('CONTENTFUL_SPACE_ID'))
-      cy.get('input#input-delivery-token').should('have.value', Cypress.env('CONTENTFUL_DELIVERY_TOKEN'))
-      cy.get('input#input-preview-token').should('have.value', Cypress.env('CONTENTFUL_PREVIEW_TOKEN'))
+
+      formItemByLabel('Space ID').should('have.value', Cypress.env('CONTENTFUL_SPACE_ID'))
+      formItemByLabel('Content Delivery API access token').should('have.value', Cypress.env('CONTENTFUL_DELIVERY_TOKEN'))
+      formItemByLabel('Content Preview API access token').should('have.value', Cypress.env('CONTENTFUL_PREVIEW_TOKEN'))
     })
 
     it('checks for required fields', () => {
-      cy.get('input#input-space-id').clear()
-      cy.get('input#input-delivery-token').clear()
-      cy.get('input#input-preview-token').clear()
+      formItemByLabel('Space ID').clear()
+      formItemByLabel('Content Delivery API access token').clear()
+      formItemByLabel('Content Preview API access token').clear()
+
       cy.get('input[type=submit]').click()
 
-      cy.get('.status-block--info').should('not.exist')
+      cy.get('.status-block--info').should('exist')
       cy.get('.status-block--success').should('not.exist')
-      cy.get('.status-block--error').should('exist')
+      cy.get('.status-block--error').should('not.exist')
 
-      cy.get('input#input-space-id').parent().children('.form-item__error-wrapper')
-        .should('exist')
-        .find('.form-item__error-message')
-        .should('contain', 'This field is required')
-      cy.get('input#input-delivery-token').parent().children('.form-item__error-wrapper')
-        .should('exist')
-        .find('.form-item__error-message')
-        .should('contain', 'This field is required')
-      cy.get('input#input-preview-token').parent().children('.form-item__error-wrapper')
-        .should('exist')
-        .find('.form-item__error-message')
-        .should('contain', 'This field is required')
+      formItemByLabel('Space ID').should('have.attr', 'required')
+      formItemByLabel('Content Delivery API access token').should('have.attr', 'required')
+      formItemByLabel('Content Preview API access token').should('have.attr', 'required')
     })
 
     it('validates field with actual client', () => {
-      cy.get('input#input-space-id').clear().type(Math.random().toString(36).substring(12))
-      cy.get('input#input-delivery-token').clear().type(Math.random().toString(36))
-      cy.get('input#input-preview-token').clear().type(Math.random().toString(36))
+      formItemByLabel('Space ID').clear().type(Math.random().toString(36).substring(12))
+      formItemByLabel('Content Delivery API access token').clear().type(Math.random().toString(36))
+      formItemByLabel('Content Preview API access token').clear().type(Math.random().toString(36))
       cy.get('input[type=submit]').click()
 
       cy.get('.status-block--info').should('not.exist')
       cy.get('.status-block--success').should('not.exist')
       cy.get('.status-block--error').should('exist')
 
-      cy.get('input#input-delivery-token').parent().children('.form-item__error-wrapper')
+      formItemByLabel('Content Delivery API access token').parent().children('.form-item__error-wrapper')
         .should('exist')
         .find('.form-item__error-message')
         .should('contain', 'Your Delivery API key is invalid.')
     })
 
     it('shows success message when valid credentials are supplied', () => {
-      cy.get('input#input-space-id').clear().type(Cypress.env('CONTENTFUL_SPACE_ID'))
-      cy.get('input#input-delivery-token').clear().type(Cypress.env('CONTENTFUL_DELIVERY_TOKEN'))
-      cy.get('input#input-preview-token').clear().type(Cypress.env('CONTENTFUL_PREVIEW_TOKEN'))
+      cy.get('.status-block')
+        .should('have.length', 1)
+        .invoke('text').then((text) => {
+          expect(text).to.contain('The example app is currently using server side stored credentials to connect to a Contentful space.')
+        })
+      formItemByLabel('Space ID').clear().type(Cypress.env('CONTENTFUL_SPACE_ID'))
+      formItemByLabel('Content Delivery API access token').clear().type(Cypress.env('CONTENTFUL_DELIVERY_TOKEN'))
+      formItemByLabel('Content Preview API access token').clear().type(Cypress.env('CONTENTFUL_PREVIEW_TOKEN'))
       cy.get('input[type=submit]').click()
 
       cy.get('.status-block--info').should('exist')
