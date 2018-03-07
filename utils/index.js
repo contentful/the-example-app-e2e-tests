e01789452630a1d091e58512301e7e12066f6ba2
Benedikt Rötsch <benedikt@contentful.com>
2018-03-07 14:02:36 +0100

    feat(empty-states): add tests for empty states (no content)

--- utils/index.js
+++ utils/index.js
@@ -15,7 +15,17 @@ function switchToQASpace () {
   cy.visit(`/?${getParams.join('&')}`)
 }
 
+function switchToEmptyStatesSpace () {
+  const getParams = [
+    `space_id=${Cypress.env('CONTENTFUL_QA_EMPTY_STATES_SPACE_ID')}`,
+    `delivery_token=${Cypress.env('CONTENTFUL_QA_EMPTY_STATES_DELIVERY_TOKEN')}`,
+    `preview_token=${Cypress.env('CONTENTFUL_QA_EMPTY_STATES_PREVIEW_TOKEN')}`
+  ]
+  cy.visit(`/?${getParams.join('&')}`)
+}
+
 module.exports = {
   formItemByLabel,
+  switchToEmptyStatesSpace,
   switchToQASpace
 }
